0232c604e12cacf3779d173c939a7b93255c1e55
Amal George Thomas <amal.george@flytxt.com>
2016-12-15 16:16:38 +0530

    Initial Commits

---
+++ scripts/firebase-sample.js
@@ -0,0 +1,9 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDhRl5a2DoMPhUPyiRjcGcKD05rZzLIOSg",
+    authDomain: "friendlychat-c7095.firebaseapp.com",
+    databaseURL: "https://friendlychat-c7095.firebaseio.com",
+    storageBucket: "friendlychat-c7095.appspot.com",
+    messagingSenderId: "1002815261499"
+  };  
+  firebase.initializeApp(config);
