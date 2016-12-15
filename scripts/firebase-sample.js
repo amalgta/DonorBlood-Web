68e0372574a22b7e41475b8fcff0ab423de1bbde
Amal George Thomas <amal.george@flytxt.com>
2016-12-15 18:24:25 +0530

    Scratch Commit

--- scripts/firebase-sample.js
+++
@@ -1,9 +0,0 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "null",
-    authDomain: "null",
-    databaseURL: "null",
-    storageBucket: "null",
-    messagingSenderId: "null"
-  };  
-  firebase.initializeApp(config);
